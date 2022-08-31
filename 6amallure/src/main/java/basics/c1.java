137edf09eb3fbdbc91b76cef513e02387aaa7b6f
hp <hp@krish>
2022-08-31 06:37:48 +0530

    first commit for java selenium

---
+++ 6amallure/src/main/java/basics/c1.java
@@ -0,0 +1,25 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+
+public class c1 {
+	
+	@Test
+	@Description("thi is first")
+	@Feature("visiting website")
+	@Severity(SeverityLevel.NORMAL)
+
+	public void t1()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Desktop\\chromedriver.exe");
+		ChromeDriver ob= new ChromeDriver();
+		ob.get("http://www.google.com");
+	}
+
+}
